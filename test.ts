a9c7c693a587f79d5c9b7c7b8fbafd81273950cc
Xaver <91993589+coding4coconut@users.noreply.github.com>
2022-06-19 20:42:08 +0200

    Aktualisiere pxt.json, main.blocks, test.ts

--- test.ts
+++ test.ts
@@ -1,17 +1,16 @@
 // Hier kann man Tests durchführen; diese Datei wird nicht kompiliert, wenn dieses Paket als Erweiterung verwendet wird.
 /*
-let Counter = 754
-let CounterSTRING = "111"
-groveOledDisplay.init()
+lgroveOledDisplay.init()
 groveOledDisplay.clearDisplay()
-groveOledDisplay.drawHLine(20, 20, 100)
-basic.showIcon(IconNames.Happy)
-groveOledDisplay.setTextXY(7, 10)
+groveOledDisplay.drawHLine(10, 10, 110)
+groveOledDisplay.drawHLine(120, 10, 110)
+groveOledDisplay.setTextXY(6, 30)
 groveOledDisplay.writeString("HELLO SUN ")
-groveOledDisplay.setTextXY(9, 5)
+groveOledDisplay.setTextXY(9, 50)
 groveOledDisplay.putNumber(432)
-groveOledDisplay.drawPixel(16, 50, 1)
-groveOledDisplay.drawVLine(0, 120, 128)
+groveOledDisplay.drawPixel(25, 64, 1)
+groveOledDisplay.drawVLine(10, 120, 110)
+groveOledDisplay.drawVLine(10, 10, 110)
 groveOledDisplay.setContrast(255)
 basic.pause(500)
 groveOledDisplay.setBackground(BckColor.WHITE)
